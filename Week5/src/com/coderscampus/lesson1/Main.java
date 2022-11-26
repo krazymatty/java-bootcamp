d748433596563e0c194fd703c8f2b2bfbb4078ab
Phil Matthews <phil@viralwebtactics.com>
2022-11-25 18:20:24 -0700

    Exercise 1 Complete

---
+++ Week5/src/com/coderscampus/lesson1/Main.java
@@ -0,0 +1,11 @@
+package com.coderscampus.lesson1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		User theUser = new User("krazymatty","myPassword", "Philip Matthews");
+		System.out.println(theUser.username);
+
+	}
+
+}
