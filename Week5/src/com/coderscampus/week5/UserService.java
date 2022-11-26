41ca637002ef2e215d186ae947bc0af45f7a303b
Phil Matthews <phil@viralwebtactics.com>
2022-11-26 12:22:25 -0700

    Created User Service - Exercise

--- Week5/src/com/coderscampus/week5/UserService.java
+++ Week5/src/com/coderscampus/week5/UserService.java
@@ -2,10 +2,11 @@ package com.coderscampus.week5;
 
 public class UserService {
 
-	public User createUser(String username, String password) {
+	public User createUser(String username, String password, String name) {
 		User user = new User();
 		user.setUsername(username);
 		user.setPassword(password);
+		user.setPassword(name);
 		return user;
 	}
 
